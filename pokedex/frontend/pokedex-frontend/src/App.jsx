28f6f757de15975b717614a85aa6568e20b702d0
EndoSeiiti <endoseiiti@gmail.com>
2025-08-21 14:54:50 -0300

    feat:add random button functionality and default pokemon sprite

--- pokedex/frontend/pokedex-frontend/src/App.jsx
+++ pokedex/frontend/pokedex-frontend/src/App.jsx
@@ -4,9 +4,19 @@ import './App.css'
 function Pokedex() {
   const [pokemonName, setPokemonName] = useState('');
   const [pokemonData, setPokemonData] = useState(null);
-  const [loading, setLoading]=useState(false)
-  const [error,setError]= useState('')
+  const [loading, setLoading]=useState(false);
+  const [error,setError]= useState('');
   
+  
+  const handleRandom = ()=>{
+    let min=1;
+    let max= 1025;
+    const randomID =Math.floor(Math.random()*(max-min+1))+ min;
+    setPokemonName(String(randomID));
+    handleSearch();
+  }
+
+
   const handleSearch = async()=>{
     if (pokemonName.trim()===''){
       setError('nada');
@@ -60,7 +70,7 @@ function Pokedex() {
             '
           >Search</button>
         </div>
-          <button className='
+          <button onClick={handleRandom} className='
             border
             border-gray-300
             w-[300px]
@@ -72,10 +82,19 @@ function Pokedex() {
             {pokemonData && (    
               <div>       
             <div className='text-2xl font-bold capitalize text-center'>{pokemonData.name}</div>
-            <img src ={pokemonData.sprite} alt={pokemonData.name} className='w-[200%] h-[200%]'/>
+            <div className='flex'>
+              <div className='flex-col '>
+                <h1 className='text-center'>Normal</h1>
+                <img src ={pokemonData.sprite} alt={pokemonData.name} className='w-48 h-48'/>
+              </div>
+              <div>
+                <h1 className='text-center'>Shiny</h1>
+                <img src ={pokemonData.sprite2} alt={pokemonData.name} className='w-48 h-48'/>            
+              </div>
+            </div>
             <div className='text-center'>
               <p>ID:{pokemonData.id}</p>
-              <p>Type:{pokemonData.types?.map (t=>t.type.name).join(',')}</p>
+              <p>Type:{pokemonData.types.map (t=>t.type.name).join(',')}</p>
               <p>Abilities:{pokemonData.abilities?.map (a=>a.ability.name).join(',')}</p>
             </div>
               </div>
